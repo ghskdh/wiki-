0bf994e8783b15008bb1f7864da1d96f71b04e0d
hhh <2258314040@qq.com>
2023-06-14 20:31:19 +0800

    2-8 增加默认配置项实例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController //一般用来返回字符串
 //@Controller  //一般用来返回页面
 public class TestController {
+
+    @Value("${test.hello:TEST}")  //冒号后面的是默认值，万一配置文件没有配置成功，就会使用这个默认值
+    private String testHello;
 
     @GetMapping("/hello")
     public String hello(){
