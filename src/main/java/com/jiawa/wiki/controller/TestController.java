6b8eabb8ce298b889428b8930ccb4fa3d63a33b3
hhh <2258314040@qq.com>
2023-06-14 20:44:08 +0800

    2-9 集成热部署

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world! " + testHello;
     }
 
     @PostMapping("/hello")
