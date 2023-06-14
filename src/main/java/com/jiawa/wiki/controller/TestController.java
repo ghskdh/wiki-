77fe08466d2994f3ec855567ca86e4751ccd90cd
hhh <2258314040@qq.com>
2023-06-14 20:10:02 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.jiawa.wiki.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController //一般用来返回字符串
 //@Controller  //一般用来返回页面
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @PostMapping("/hello")
+    public String hello(String name){
+        return "hello map!" + name;
     }
 
 }
